ac2b6afaec4aa4b333bd50d44368644df8ad1e55
Oskar Podrucki <118886547+oskarpodrucki@users.noreply.github.com>
2024-10-04 15:49:39 +0200

    ...

--- src/components/NameList/index.js
+++ src/components/NameList/index.js
@@ -2,7 +2,7 @@
 
 export default function NameList({ name, pay = "yes", ok }) {
     return (
-        <div className={`flex justify-center items-center w-full md:w-1/2 border p-4 ${ok ? 'bg-black' : 'bg-white'}`}>
+        <div className={`flex justify-center items-center h-[20px] w-[500px] border p-4 ${ok ? 'bg-black' : 'bg-white'}`}>
             <h1 className={`${pay === "no" ? 'text-red-500' : 'text-emerald-500'} text-xl`}>{name}</h1>
         </div>
     );
